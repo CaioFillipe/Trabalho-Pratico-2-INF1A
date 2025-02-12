09855367d418948a95c1fd9a2305f288caa76d6f
CaioFillipe <162356041+CaioFillipe@users.noreply.github.com>
2025-02-12 15:57:20 -0300

    Update sistemaregistro.php

--- backend_php/sistemaregistro.php
+++ backend_php/sistemaregistro.php
@@ -8,9 +8,11 @@ if ($_POST) {
     $sql = "INSERT INTO usuarios (nome, senha) VALUES ('$nome', '$senha')"; //INSERE O NOVO USUÁRIO NO BANCO DE DADOS
     
     if ($conexao->query($sql)) { //VERIFICA SE A INSERÇÃO OBTEVE SUCESSO, SE SIM, DIRECIONA O PARA A PÁGINA DE LOGIN
-        header("Location: entrar.php?sucesso=1");
+        header("Location: sistemalogin.php?sucesso=1");
     } else { //SE NÃO, AVISA QUE DEU ERRO
-        echo "<script>alert('Deu errado! Tente outro nome.');</script>";
+        echo "<script>
+            alert('Deu errado! Tente outro nome.');
+        </script>";
     }
 }
 ?>
