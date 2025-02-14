04e21dbf306ae50644a686a355583aa79725aea3
Arturdias2 <160796242+Arturdias2@users.noreply.github.com>
2025-02-13 23:09:14 -0300

    Delete backend directory

--- backend/saidaconta.php
+++
@@ -1,6 +0,0 @@
-<?php
-//SISTEMA DE SAÍDA DA CONTA DO USUÁRIO 
-    session_start();
-    session_destroy();   // DESTRÓI A SESSÃO ATUAL
-    header("Location: ../paginalogin.html"); //DIRECIONA O USUÁRIO PARA A PÁGINA DE REGISTRO
-?>
